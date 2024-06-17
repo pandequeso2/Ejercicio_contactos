a91c97b6fc88ec3eb0d4b2a193565bd67ea6357b
Benjamin <ben.arayag@duocuc.cl>
2024-06-16 22:37:01 -0400

    Opción 2 terminada

--- Ejercicio_contactos.py
+++ Ejercicio_contactos.py
@@ -9,6 +9,7 @@ while True:
     os.system('cls')
     opc=int(input('Ingrese una opcion: '))
     if opc==1:
+        print('Agregar contactos: ')
         nombre=input('Ingresa tu nombre: ')
         numero_telefonico=int(input('Ingresa tu numero telefonico (sin el +): '))
         correo=input('Ingrese su correo electronico: ')
@@ -17,7 +18,12 @@ while True:
         time.sleep(3)
         print('Contacto agregado con exito...')
     elif opc==2:
-        pass
+        print('Mostrar contactos: ')
+        if len(contactos) == 0:
+            print('No tienes contactos en tu lista, prueba ir a la opción 1.')
+        else:
+            for c in contactos:
+                print(f'Nombre: {c[0]}\nNumero telefonico: {c[1]}\nCorreo: {c[2]} ')
     elif opc==3:
         pass
     else:
